8fe49d394929de4764df22abc02d50ad83e1e70f
block_given? <block_given@outlook.com>
2014-10-09 03:56:57 +0900

    Fix message reply
    ooopsly naming

--- lib/ruboty/actions/opening_sentence.rb
+++ lib/ruboty/actions/opening_sentence.rb
@@ -10,13 +10,12 @@ module Ruboty
       def call
         json = open(AOZORA_RANDOM_BOOK_URL).read rescue return
         book = JSON.parse(json) rescue return
-        message = [
+        message.reply [
           "@#{message.from_name}",
           "作品名: #{book['title']}",
           book['opening_sentence'],
           book['url']
         ].join($/)
-        message.reply(message)
       end
     end
   end
